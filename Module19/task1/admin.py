ad74699cd463e63267339c08a5215acac29345a9
irikuz <i-iri.kuz@yandex.ru>
2024-12-12 12:52:29 +0300

    Add files via upload

--- Module19/task1/admin.py
+++ Module19/task1/admin.py
@@ -1,6 +1,28 @@
 from django.contrib import admin
 from .models import Buyer, Game
 
+
 # Register your models here.
-admin.site.register(Buyer)
-admin.site.register(Game)
+@admin.register(Buyer)
+class Admin_Buyer(admin.ModelAdmin):
+    list_display = ('name', 'balance', 'age')
+    search_fields = ('name',)
+    list_filter = ('balance', 'age',)
+    fields = [('name', 'balance', 'age')]
+
+
+@admin.register(Game)
+class Admin_Game(admin.ModelAdmin):
+    list_display = ('title', 'cost', 'size', 'age_limited')
+    search_fields = ('title',)
+    list_filter = ('cost', 'size', 'age_limited',)
+    fieldsets = (
+        ('Game Info', {
+            'fields': (('title', 'cost'),)
+        }),
+        ('Game Description', {
+            'fields': ('description', ('size', 'age_limited'),)
+        }),
+    )
+
+
